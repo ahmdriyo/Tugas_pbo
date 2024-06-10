485495f06b1e21e9a211590c8008222a10d9b763
ahmdriyo <ahmdriyo.tbn.com>
2024-06-10 15:53:03 +0800

    first commit

---
+++ src/crud/Koneksi.java
@@ -0,0 +1,246 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author ahmad
+ */
+public class Koneksi {
+    private String database_name = "ahmadriyokusuma_2210010085";
+    private String username = "root";
+    private String password = "";
+    public Connection koneksiDB; 
+    
+    public Koneksi(){
+    
+        try {  
+            String lokasi = "jdbc:mysql://localhost/" + database_name ;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            koneksiDB = DriverManager.getConnection(lokasi, username, password);
+            
+            System.out.println("Database Terkoneksi");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());          
+        }
+        
+    }
+    //travel_agen
+    public void SimpanTravelAgen (String id, String nama_agen, String slogan, String deskripsi, String nama_domain){
+        try {
+            String sql = "INSERT INTO travel_agen (id, nama_agen, slogan, deskripsi,nama_domain) VALUE (?,?,?,?,?)";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, id);
+            perintah.setString(2, nama_agen);
+            perintah.setString(3, slogan);
+            perintah.setString(4, deskripsi);
+            perintah.setString(5, nama_domain);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Disimpan");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void UbahTravelAgen(String id, String nama_agen, String slogan, String deskripsi, String nama_domain){
+        try {
+            String sql = "UPDATE travel_agen SET name_agen=?, slogan=?, deskripsi=?,nama_domain=? where id=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, id);
+            perintah.setString(2, nama_agen);
+            perintah.setString(3, slogan);
+            perintah.setString(4, deskripsi);
+            perintah.setString(5, nama_domain);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Diubah");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void HapusTravelAgen(String id){
+        try {
+            String sql = "DELETE FROM travel_agen where id=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+
+            perintah.setString(1, id);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Dihapus ");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    // travel_pelanggan
+    public void SimpanTravelPelanggan (String id, String nama_pelanggan, String jenis_kelamin, String umur, String no_hp){
+        try {
+            String sql = "INSERT INTO travel_pelanggan (id, nama_pelanggan, jenis_kelamin, umur,no_hp) VALUE (?,?,?,?,?)";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, id);
+            perintah.setString(2, nama_pelanggan);
+            perintah.setString(3, jenis_kelamin);
+            perintah.setString(4, umur);
+            perintah.setString(5, no_hp);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Disimpan");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void UbahTravelPelanggan(String id, String nama_pelanggan, String jenis_kelamin, String umur, String no_hp){
+        try {
+            String sql = "UPDATE travel_pelanggan SET nama_pelanggan=?, jenis_kelamin=?, umur=?,no_hp=? where id=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, id);
+            perintah.setString(2, nama_pelanggan);
+            perintah.setString(3, jenis_kelamin);
+            perintah.setString(4, umur);
+            perintah.setString(5, no_hp);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Diubah");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void HapusTravelPelanggan(String id){
+        try {
+            String sql = "DELETE FROM travel_pelanggan where id=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+
+            perintah.setString(1, id);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Dihapus ");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    
+    // travel_tiket
+    public void SimpanTravelTiket (String kode_tiket, String tgl_berangkat, String dari, String kota_tujuan, String jam, String harga){
+        try {
+            String sql = "INSERT INTO travel_tiket (kode_tiket, tgl_berangkat, dari, kota_tujuan,jam,harga) VALUE (?,?,?,?,?,?)";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, kode_tiket);
+            perintah.setString(2, tgl_berangkat);
+            perintah.setString(3, dari);
+            perintah.setString(4, kota_tujuan);
+            perintah.setString(5, jam);
+            perintah.setString(6, harga);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Disimpan");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void UbahTravelTiket(String kode_tiket, String tgl_berangkat, String dari, String kota_tujuan, String jam, String harga){
+        try {
+            String sql = "UPDATE travel_tiket SET tgl_berangkat=?, dari=?, kota_tujuan=?,jam=?,harga=? where kode_tiket=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, kode_tiket);
+            perintah.setString(2, tgl_berangkat);
+            perintah.setString(3, dari);
+            perintah.setString(4, kota_tujuan);
+            perintah.setString(5, jam);
+            perintah.setString(6, harga);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Diubah");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void HapusTravelTiket(String id){
+        try {
+            String sql = "DELETE FROM travel_tiket where kode_tiket=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+
+            perintah.setString(1, id);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Dihapus ");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    //travel_pegawai
+    public void SimpanTravePegawai (String nik, String id, String nama, String jenis_kelamin, String tgl_lahir, String alamat, String kontak, String jabatan){
+        try {
+            String sql = "INSERT INTO travel_pegawai (nik, id, nama, jenis_kelamin, tgl_lahir,alamat,kontak,jabatan) VALUE (?,?,?,?,?,?,?,?)";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, nik);
+            perintah.setString(2, id);
+            perintah.setString(3, nama);
+            perintah.setString(4, jenis_kelamin);
+            perintah.setString(5, tgl_lahir);
+            perintah.setString(6, alamat);
+            perintah.setString(7, kontak);
+            perintah.setString(8, jabatan);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Disimpan");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void UbahTravelPegawai(String nik, String id, String nama, String jenis_kelamin, String tgl_lahir, String alamat, String kontak, String jabatan){
+        try {
+            String sql = "UPDATE travel_pegawai SET id=?, nama=?, jenis_kelamin=?,tgl_lahir=?,alamat=?,kontak=? ,jabatan=?  where nik=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+            
+            perintah.setString(1, nik);
+            perintah.setString(2, id);
+            perintah.setString(3, nama);
+            perintah.setString(4, jenis_kelamin);
+            perintah.setString(5, tgl_lahir);
+            perintah.setString(6, alamat);
+            perintah.setString(7, kontak);
+            perintah.setString(8, jabatan);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Diubah");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+    public void HapusTravelPegawai(String id){
+        try {
+            String sql = "DELETE FROM travel_pegawai where id=?";
+            PreparedStatement perintah = koneksiDB.prepareStatement(sql);
+
+            perintah.setString(1, id);
+            
+            perintah.executeUpdate();
+            System.out.println("Berhasil Dihapus ");
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+    
+}
---
+++ src/tugaspbo/TugasPBO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspbo;
+
+import crud.Koneksi;
+
+/**
+ *
+ * @author ahmad
+ */
+public class TugasPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Koneksi myObject = new Koneksi();
+//         myObject.Ubah("221009", "momo", "923232");
+//         myObject.SimpanTravelAgen("2332", "yoko", "hidupsehat", "keren", "yoko.com");
+//         myObject.UbahTravelAgen("2332", "yuno", "hidupsehat", "keren", "yoko.com");
+//         myObject.HapusTravelAgen("3232");
+
+//         myObject.SimpanTravelPelanggan("2121", "opoik", "laki-laki", "32", "032023022");
+//         myObject.UbahTravelPelanggan("2121", "opossssik", "laki-laki", "32", "032023022");
+//         myObject.HapusTravelPelanggan("2121");
+            
+//         myObject.SimpanTravelTiket("214343", "2022-03-04", "Banjarmasin", "Kapuas", "18:00", "3000");
+//         myObject.UbahTravelTiket("214343", "2022-03-04", "Banjarmasin", "Kapuas", "18:00", "3222000");
+//         myObject.HapusTravelTiket("214343");
+
+//         myObject.SimpanTravePegawai("33", "2", "santo", "laki-laki", "2002-03-05", "tamban", "09223222", "admin");
+//         myObject.UbahTravelPegawai("33", "2", "santo", "laki-laki", "2002-03-05", "Anjir", "09223222", "admin");
+//         myObject.HapusTravelPegawai("33");
+        // TODO code application logic here
+    }
+    
+}
